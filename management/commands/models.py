23e03ebcbc885f543a24e01f1f836c13f338dc96
Денис Лисиця <Denys.Lysysia@cs.khpi.edu.ua>
2023-04-27 21:19:41 +0300

    dd

---
+++ management/commands/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    subject = models.CharField(max_length=255)
+    years_of_experience = models.IntegerField()
+
+    objects = models.Manager()
